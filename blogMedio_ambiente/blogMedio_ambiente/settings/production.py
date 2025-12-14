d15cfc699a16cbabd699e8291cd832c632dc2a92
20sa-brina <saabri20@gmail.com>
2025-12-14 17:12:23 -0300

    Config producción: CSRF y SECRET_KEY por variable de entorno

--- blogMedio_ambiente/blogMedio_ambiente/settings/production.py
+++ blogMedio_ambiente/blogMedio_ambiente/settings/production.py
@@ -1,11 +1,17 @@
 import os
 from .base import *
 
-SECRET_KEY = 'django-insecure-8(d7#byqkb251$+)0k0kcwwjmt=x7ztx7fqj_a)if$9@j41mbq'
+# SECRET_KEY seguro usando variable de entorno
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-temporal"  # solo fallback, no usar en producción real
+)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['blog-huellas-conscientes.onrender.com']
+ALLOWED_HOSTS = ["blog-huellas-conscientes.onrender.com"]
 
-CSRF_TRUSTED_ORIGINS = ['https://blog-huellas-conscientes.onrender.com']
+# Permite que los POST desde tu dominio pasen la verificación CSRF
+CSRF_TRUSTED_ORIGINS = ["https://blog-huellas-conscientes.onrender.com"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
