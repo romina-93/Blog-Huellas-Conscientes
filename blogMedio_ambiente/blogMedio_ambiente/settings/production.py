be92819dec3ead1e01af56a3349b7e31207b7cb3
20sa-brina <saabri20@gmail.com>
2025-12-14 10:53:10 -0300

    Configura ALLOWED_HOSTS para producción y entorno local

---
+++ blogMedio_ambiente/blogMedio_ambiente/settings/production.py
@@ -0,0 +1,8 @@
+import os
+from .base import *
+
+SECRET_KEY = 'django-insecure-8(d7#byqkb251$+)0k0kcwwjmt=x7ztx7fqj_a)if$9@j41mbq'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['blog-huellas-conscientes.onrender.com', 'localhost', '127.0.0.1']
