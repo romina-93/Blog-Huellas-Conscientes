ef794f993a72852c62ce5c25bd9f7865e16ff667
20sa-brina <saabri20@gmail.com>
2025-12-14 15:30:58 -0300

    Arreglado CSRF para producción en Render

--- blogMedio_ambiente/blogMedio_ambiente/settings/production.py
+++ blogMedio_ambiente/blogMedio_ambiente/settings/production.py
@@ -3,6 +3,9 @@ from .base import *
 
 SECRET_KEY = 'django-insecure-8(d7#byqkb251$+)0k0kcwwjmt=x7ztx7fqj_a)if$9@j41mbq'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['blog-huellas-conscientes.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['blog-huellas-conscientes.onrender.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://blog-huellas-conscientes.onrender.com']
+
