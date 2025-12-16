34393fb525d248ffb93bb1894a670adc7a2d5c7e
20sa-brina <saabri20@gmail.com>
2025-12-16 11:36:16 -0300

    Set production settings in wsgi

--- blogMedio_ambiente/blogMedio_ambiente/wsgi.py
+++ blogMedio_ambiente/blogMedio_ambiente/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'blogMedio_ambiente.settings'
+    'blogMedio_ambiente.settings.production'
 )
 
 application = get_wsgi_application()
