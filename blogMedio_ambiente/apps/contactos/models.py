015f204d0e9749a33efb7065b483a439ed201eb7
20sa-brina <saabri20@gmail.com>
2025-12-16 10:10:43 -0300

    Mensaje del cambio

--- blogMedio_ambiente/apps/contactos/models.py
+++ blogMedio_ambiente/apps/contactos/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+
+from django.db import models
 from django.utils import timezone
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
+    email = models.CharField()
     asunto = models.CharField(max_length=200)
     mensaje = models.TextField()
     fecha_envio = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.nombre} - {self.asunto}"
-
