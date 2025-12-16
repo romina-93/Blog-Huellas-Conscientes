e702e738bdcd44a532431b959cc806bc922ee11c
20sa-brina <saabri20@gmail.com>
2025-12-16 15:31:10 -0300

    cambio en production

--- blogMedio_ambiente/blogMedio_ambiente/settings/production.py
+++ blogMedio_ambiente/blogMedio_ambiente/settings/production.py
@@ -1,17 +1,12 @@
 import os
 from .base import *
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-temporal"  
-)
+SECRET_KEY = 'django-insecure-8(d7#byqkb251$+)0k0kcwwjmt=x7ztx7fqj_a)if$9@j41mbq'
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
 	"blog-huellas-conscientes.onrender.com",
-	"localhost",
-    "127.0.0.1",
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
