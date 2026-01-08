4bd93cbf017ceaca6096b64788602d689e7e4ff2
20sa-brina <saabri20@gmail.com>
2026-01-08 12:18:46 -0300

    Elimina CSRF_USE_SESSIONS y corrige error 400

--- blogMedio_ambiente/blogMedio_ambiente/settings/base.py
+++ blogMedio_ambiente/blogMedio_ambiente/settings/base.py
@@ -11,7 +11,6 @@ LOGIN_REDIRECT_URL = reverse_lazy('path_inicio')
 LOGOUT_REDIRECT_URL = reverse_lazy('path_inicio')
 LOGIN_URL = reverse_lazy('usuarios:path_login')
 
-CSRF_USE_SESSIONS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
