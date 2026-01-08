041c3bbd587c4c9d3568467d35286d56f5cde4cb
20sa-brina <saabri20@gmail.com>
2026-01-08 13:29:51 -0300

    Configura archivos estáticos con WhiteNoise

--- blogMedio_ambiente/blogMedio_ambiente/settings/base.py
+++ blogMedio_ambiente/blogMedio_ambiente/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	"whitenoise.middleware.WhiteNoiseMiddleware",  # 👈 este
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,8 +91,9 @@ USE_TZ = True
 # -----------------------
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
